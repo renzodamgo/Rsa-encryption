72ee6369fbe8449fb9dfbe2335ac0a3963f32615
Renzo Damian <renzo.damian.go@gmail.com>
2019-08-19 17:47:43 -0500

    funcional encriptar

--- ejecutable.py
+++ ejecutable.py
@@ -3,8 +3,42 @@ from tkinter import ttk
 
 def calculate(*args):
     try:
-        #value = float(p.get())
-        encript.set("hola")
+        _p = int(p.get())
+        _q = int(q.get())
+        _d = int(d.get())
+        _word = str(word.get())
+
+        n = _p*_q
+        En = (_p-1)*(_q-1)
+        euler = 0
+
+        while True:
+            mod = (_d * euler) % En
+            if mod == 1:
+                break
+            euler += 1
+
+        cPriv = "clave privada("+ str(n)+"," +str(_d)+")"
+        cPub = "clave publica("+ str(n)+"," +str(euler)+")"
+        clv1.set(cPriv)
+        clv2.set(cPub)
+
+
+        palabra = list(_word)
+
+        end_word = ""
+        
+        for caracter in palabra:
+            c = ord(caracter)
+            encrypt = (c ** euler)%n
+            end_word += chr(encrypt)
+
+        encript.set(end_word)
+
+
+
+
+
     except ValueError:
         pass
 
@@ -21,6 +55,8 @@ q = StringVar()
 d = StringVar()
 word = StringVar()
 
+clv1 = StringVar()
+clv2 = StringVar()
 encript = StringVar()
 
 p_entry = ttk.Entry(mainframe, width=12, textvariable=p)
@@ -33,8 +69,10 @@ word_entry = ttk.Entry(mainframe, width=12, textvariable=word)
 word_entry.grid(column=2, row=4, sticky=(W, E))
 
 
-ttk.Label(mainframe, textvariable=encript).grid(column=2, row=5, sticky=(W, E))
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=2, row=6, sticky=W)
+ttk.Label(mainframe, textvariable=clv1).grid(column=2, row=5, sticky=(W, E))
+ttk.Label(mainframe, textvariable=clv2).grid(column=2, row=6, sticky=(W, E))
+ttk.Label(mainframe, textvariable=encript).grid(column=2, row=7, sticky=(W, E))
+ttk.Button(mainframe, text="Encriptar", command=calculate).grid(column=2, row=8, sticky=W)
 
 ttk.Label(mainframe, text="p:").grid(column=1, row=1)
 ttk.Label(mainframe, text="q:").grid(column=1, row=2)
