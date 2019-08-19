8ba90bd5b3adffa6640fe610627911bd0d984d65
Renzo Damian <renzo.damian.go@gmail.com>
2019-08-18 20:07:34 -0500

    inicio

---
+++ prueba.py
@@ -0,0 +1,44 @@
+
+
+p = int(input("p: "))
+q = int(input("q: "))
+d = int(input("d: "))
+
+n = p*q
+En = (p-1)*(q-1)
+euler = 0
+
+while True:
+    mod = (d * euler) % En
+    if mod == 1:
+        break
+    euler += 1
+
+print("clave privada(", n, "," ,d,")" )
+print("clave publica(", n, "," ,euler,")" )
+
+
+word = input("word: ")
+palabra = list(word)
+#print(palabra)
+end_word = ""
+dec_word = ""
+for caracter in palabra:
+    c = ord(caracter)
+    encrypt = (c ** euler)%n
+    end_word += chr(encrypt)
+    
+    #print(encrypt,'\t',chr(encrypt) )
+    
+    #print(decrypt,'\t',chr(decrypt) )
+    #print(chr((c ** euler)%n))
+print (end_word)
+
+
+for caracter in end_word:
+    c = ord(caracter)
+    decrypt = (c ** d)%n    
+    dec_word += chr(decrypt)
+
+print (dec_word)
+
